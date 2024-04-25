6fb102b1500fc95811060e498b2401bd9443ebb6
JOSH SPENCE <joshua.spence@hotmail.co.uk>
2024-04-25 12:31:27 +0100

    git issues sorted, hashrouter implemented

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppContainer from "./AppContainer/AppContainer.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AppContainer />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
--- src/types/types.ts
+++ src/types/types.ts
@@ -4,15 +4,15 @@ export type Beer = {
   tagline: string;
   first_brewed: string;
   description: string;
-  image_url: string | null;
-  abv: number | null;
-  ibu: number | null;
-  target_fg: number | null;
-  target_og: number | null;
-  ebc: number | null;
-  srm: number | null;
-  ph: number | null;
-  attenuation_level: number | null;
+  image_url: any;
+  abv: any;
+  ibu: any;
+  target_fg: any;
+  target_og: any;
+  ebc: any;
+  srm: any;
+  ph: any;
+  attenuation_level: any;
   volume: Volume;
   boil_volume: Volume;
   method: Method;
@@ -52,10 +52,10 @@ type Fermentation = {
 
 type MashTemp = {
   temp: Volume;
-  duration: number | null;
+  duration: any;
 };
 
 type Volume = {
-  value: number | null;
+  value: any;
   unit: string;
 };
